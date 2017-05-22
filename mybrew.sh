6f78f9deb062e680cd2bd21af65fcbcd5b54d173
Lim Swee Tat <sweetat.lim@finantix.com>
2017-05-22 14:33:10 +0800

    Adding mybrew for homebrew packages for myself

---
+++ mybrew.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install my packages that I need installed
+brew install nvm 
+brew install jenv 
+brew install rbenv 
+
+# Install from casks
+brew cask install docker
+brew cask install franz
+brew cask install google-chrome-beta
+brew cask install google-drive
+brew cask install iterm2
+brew cask install java6 
+brew cask install java7 
+brew cask install java 
+brew cask install onedrive
+brew cask install skype
+brew cask install skype-for-business
+brew cask install spectacle 
+brew cask install vimr
+
+# Source for jenv from ~/.extra
+source ~/.extra
+jenv add /Library/Java/JavaVirtualMachines/$( ls /Library/Java/JavaVirtualMachines | grep 1.6 )/Contents/Home
+jenv add /Library/Java/JavaVirtualMachines/$( ls /Library/Java/JavaVirtualMachines | grep 1.7 )/Contents/Home
+jenv add /Library/Java/JavaVirtualMachines/$( ls /Library/Java/JavaVirtualMachines | grep 1.8 )/Contents/Home
+
+# Install neovim
+brew tap neovim/neovim
+brew install neovim
+
+# Install fonts
+brew tap caskroom/fonts
+brew cask install font-arial
+brew cask install font-awesome-terminal-fonts 
+brew cask install font-fontawesome
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install iterm shell integration
+curl -L https://iterm2.com/misc/install_shell_integration_and_utilities.sh | bash
+
+# Remove outdated versions from the cellar.
+brew cleanup
