1a42d58ce249843a9918f351a5ce9829be784670
Teo Soo Pheng <spteo@stlim.net>
2019-03-15 21:15:22 +0800

    Adding this subtitle for everything in Hollywood

--- bin/mvAnime.sh
+++ bin/mvAnime.sh
@@ -7,6 +7,22 @@ KIDS_DIR=${MOVIE_DIR}/kids
 
 ANIME_PREFIX=('HorribleSubs' 'DeadFish' 'PAS' 'UTW' 'Anon' 'anon')
 
+move_file_to_dir() {
+  FILE_NAME=$1
+  MY_DIR=$2
+  MOVIE_NAME=$(basename -- $FILE_NAME)
+  FIRST_CHAR=${MOVIE_NAME:0:1}
+  THE_DETECT=${MOVIE_NAME:0:3}
+  THE_DETECT_UPPPER=$(echo ${THE_DETECT} | tr '[:lower:]' '[:upper:]')
+  if [ "$THE_DETECT_UPPPER" = "THE" ]; then
+    FIRST_CHAR=${MOVIE_NAME:4:1}
+  fi
+  [ ! -d ${HOLLY_DIR}/${FIRST_CHAR}/ ] && mkdir ${HOLLY_DIR}/${FIRST_CHAR}
+  mv ${FILE_NAME} ${HOLLY_DIR}/${FIRST_CHAR}/
+  mv ${FILE_NAME/.mp4/.srt} ${HOLLY_DIR}/${FIRST_CHAR}/
+  mv ${FILE_NAME/.mkv/.srt} ${HOLLY_DIR}/${FIRST_CHAR}/
+}
+
 [ ! -d ${ANIME_DIR} ] && mkdir -p ${ANIME_DIR} 
 for i in {0..9} {A..Z}; do
   ANIME_I=${ANIME_DIR}/${i}
@@ -40,34 +56,18 @@ IFS=$'\012'
 for i in ${MOVIE_DIR}/*\[YTS.A*\] ${MOVIES_BRACKET[*]} ${MOVIE_DIR}/*.BluRay.* ${MOVIE_DIR}/*.WEBRip.*; do
   for j in `find ${i} -name "*.mp4" -o -name "*.mkv"`; do
     subliminal download -l en -s "${j}"
-    MOVIE_NAME=$(basename $j)
-    FIRST_CHAR=${MOVIE_NAME:0:1}
-    THE_DETECT=${MOVIE_NAME:0:3}
-    THE_DETECT_UPPPER=$(echo ${THE_DETECT} | tr '[:lower:]' '[:upper:]')
-    if [ "$THE_DETECT_UPPPER" = "THE" ]; then
-      FIRST_CHAR=${MOVIE_NAME:4:1}
-    fi
-    [ ! -d ${HOLLY_DIR}/${FIRST_CHAR}/ ] && mkdir ${HOLLY_DIR}/${FIRST_CHAR}
-    mv ${j} ${HOLLY_DIR}/${FIRST_CHAR}/
-    mv ${j/.mp4/.srt} ${HOLLY_DIR}/${FIRST_CHAR}/
-    mv ${j/.mkv/.srt} ${HOLLY_DIR}/${FIRST_CHAR}/
+		move_file_to_dir "${j}" "${MOVIE_DIR}"
     if [ ! -f "${j}" ]; then
       rm -rf "${i}"
     fi
   done
 done
 
-for i in `find ${KIDS_DIR} -maxdepth 1 -name "*.mp4" -o -name "*.mkv"`; do
-  subliminal download -l en -s "${i}"
-  MOVIE_NAME=$(basename $i)
-  FIRST_CHAR=${MOVIE_NAME:0:1}
-  THE_DETECT=${MOVIE_NAME:0:3}
-  THE_DETECT_UPPPER=$(echo ${THE_DETECT} | tr '[:lower:]' '[:upper:]')
-  if [ "${THE_DETECT_UPPER}" = "THE" ]; then
-    FIRST_CHAR=${MOVIE_NAME:4:1}
+for i in `find ${KIDS_DIR} -maxdepth 2 -name "*.mp4" -o -name "*.mkv"` `find ${HOLLY_DIR} -maxdepth 2 -name "*.mp4" -o -name "*.mkv"`; do
+  MOVIE_NAME=$(basename -- $i)
+  SRT_FILE=${MOVIE_NAME%.*}.srt
+  if [ -f ${SRT_FILE} ]; then
+    subliminal download -l en -s "${i}"
+  	move_file_to_dir "${j}" "$(dirname ${j})"
   fi
-  [ ! -d ${KIDS_DIR}/${FIRST_CHAR}/ ] && mkdir ${KIDS_DIR}/${FIRST_CHAR}
-  mv ${i} ${KIDS_DIR}/${FIRST_CHAR}/
-  mv ${i/.mp4/.srt} ${KIDS_DIR}/${FIRST_CHAR}/
-  mv ${i/.mkv/.srt} ${KIDS_DIR}/${FIRST_CHAR}/
 done
