72a214eee23325df19f8d0bd8399de86590d57e2
Lim Swee Tat <sweetat+github@xfers.com>
2020-03-25 08:37:05 +0800

    Fixed mvAnime

--- bin/mvAnime.sh
+++ bin/mvAnime.sh
@@ -5,22 +5,29 @@ ANIME_DIR=${DOWNLOAD_DIR}/Anime
 HOLLY_DIR=${DOWNLOAD_DIR}/Hollywood
 KIDS_DIR=${DOWNLOAD_DIR}/kids
 
-ANIME_PREFIX=('HorribleSubs' 'DeadFish' 'PAS' 'UTW' 'Anon' 'anon' 'Over-Time' 'FLsnow' 'Some-Stuffs' 'AnimeRG')
+ANIME_PREFIX=('HorribleSubs' 'DeadFish' 'PAS' 'UTW' 'Anon' 'anon' 'Over-Time' 'FLsnow' 'Some-Stuffs' 'AnimeRG', 'Judas')
 
 move_file_to_dir() {
-  FILE_NAME=$1
+  FULL_FILENAME=$1
   MY_DIR=$2
-  MOVIE_NAME=$(basename -- $FILE_NAME)
-  FIRST_CHAR=${MOVIE_NAME:0:1}
-  THE_DETECT=${MOVIE_NAME:0:3}
-  THE_DETECT_UPPPER=$(echo ${THE_DETECT} | tr '[:lower:]' '[:upper:]')
-  if [ "$THE_DETECT_UPPPER" = "THE" ]; then
-    FIRST_CHAR=${MOVIE_NAME:4:1}
+  # https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash/965072#965072
+  MOVIE_FILENAME="${FULL_FILENAME##*/}"
+  MOVIE_DIR="${FULL_FILENAME:0:${#FULL_FILENAME} - ${#MOVIE_FILENAME}}"
+  MOVIE_FILEBASE="${MOVIE_FILENAME%.[^.]*}"
+  SRT_FILE="${MOVIE_DIR}${MOVIE_FILEBASE}.srt"
+  FIRST_CHAR=${MOVIE_FILENAME:0:1}
+  # Removing the "The" section of the filename
+  if [ "${FIRST_CHAR}" == "T" ]; then
+    THE_DETECT=${MOVIE_FILENAME:0:3}
+    THE_DETECT_UPPPER=$(echo ${THE_DETECT} | tr '[:lower:]' '[:upper:]')
+    if [ "$THE_DETECT_UPPPER" = "THE" ]; then
+      FIRST_CHAR=${MOVIE_FILENAME:4:1}
+    fi
   fi
+  # End
   [ ! -d ${MY_DIR}/${FIRST_CHAR}/ ] && mkdir ${MY_DIR}/${FIRST_CHAR}
-  mv ${FILE_NAME} ${MY_DIR}/${FIRST_CHAR}/
-  mv ${FILE_NAME/.mp4/.srt} ${MY_DIR}/${FIRST_CHAR}/
-  mv ${FILE_NAME/.mkv/.srt} ${MY_DIR}/${FIRST_CHAR}/
+  mv ${FULL_FILENAME} ${MY_DIR}/${FIRST_CHAR}/
+  mv ${SRT_FILE} ${MY_DIR}/${FIRST_CHAR}/ 2>/dev/null
 }
 
 move_anime_to_dir() {
@@ -50,18 +57,27 @@ move_to_holly() {
         MOVIES_BRACKET[$count]=${j}
   done
   echo ${MOVIES_BRACKET[*]}
-  
+
   for i in \
     ${DOWNLOAD_DIR}/*\[YTS.*\] \
     ${MOVIES_BRACKET[*]} \
     ${DOWNLOAD_DIR}/*.BluRay.* \
     ${DOWNLOAD_DIR}/*.WEBRip.*; do
-    for j in $(gfind ${i} -name "*.mp4" -o -name "*.mkv" -o -name "*.avi"); do
+    for j in $(gfind ${i} -name "*.mp4" -o -name "*.mkv" -o -name "*.avi" 2> /dev/null); do
+      # https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash/965072#965072
+      MOVIE_FILENAME="${j##*/}"
+      MOVIE_DIR="${j:0:${#j} - ${#MOVIE_FILENAME}}"
+      MOVIE_FILEBASE="${MOVIE_FILENAME%.[^.]*}"
+      SRT_FILE="${MOVIE_DIR}${MOVIE_FILEBASE}.srt"
+      if [ -f "${SRT_FILE}" ]; then
+        SRT_SIZE=$(gstat -c%s "${SRT_FILE}")
+        if [ ${SRT_SIZE} -lt 500 ]; then
+          rm -rf "${SRT_FILE}"
+        fi
+      fi
       subliminal download -l en -s "${j}"
   		move_file_to_dir "${j}" "${HOLLY_DIR}"
-      if [ ! -f "${j}" ]; then
-        rm -rf "${i}"
-      fi
+      rm -rf "${i}"
     done
   done
 }
@@ -70,16 +86,28 @@ get_dir_subs() {
   MY_DIR=$1
   IFS=$'\012'
   for i in $(gfind ${MY_DIR} -maxdepth 2 -name "*.mp4" -o -name "*.mkv" -o -name "*.avi"); do
-    MOVIE_NAME=$(basename -- $i)
-    SRT_FILE=${MOVIE_NAME%.*}.srt
-    if [ -f ${SRT_FILE} ]; then
+    # https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash/965072#965072
+    MOVIE_FILENAME="${i##*/}"
+    MOVIE_DIR="${i:0:${#i} - ${#MOVIE_FILENAME}}"
+    MOVIE_FILEBASE="${MOVIE_FILENAME%.[^.]*}"
+    SRT_FILE="${MOVIE_DIR}${MOVIE_FILEBASE}.srt"
+    if [ ! -f "${SRT_FILE}" ]; then
       subliminal download -l en -s "${i}"
     	move_file_to_dir "${i}" "${MY_DIR}"
     fi
   done
+  for i in $(gfind ${MY_DIR} -name "*.srt" -size 1000c); do
+    rm -rf ${i}
+    subliminal download -l en -s "${i/.srt/.mp4}"
+  done
 }
+
+if [ ! -d ${DOWNLOAD_DIR} ]; then
+  echo "${DOWNLOAD_DIR} not found"
+  exit 1
+fi
 
 move_anime_to_dir
 move_to_holly
-get_dir_subs ${KIDS_DIR}
+#get_dir_subs ${KIDS_DIR}
 get_dir_subs ${HOLLY_DIR}
