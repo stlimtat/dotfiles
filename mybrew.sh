93d9341687d271c65070b3560f280b77b54f32c8
Lim Swee Tat <sweetat@withevo.com>
2022-06-02 14:56:09 +0800

    Fixing the scripts and tested working

--- mybrew.sh
+++ mybrew.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 #
 # Install command-line tools using Homebrew.
-if [ ! -d /usr/local/Homebrew ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+if [[ ! -d /usr/local/Homebrew ]]; then
+    echo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 #
 # Make sure we’re using the latest Homebrew.
@@ -20,35 +20,30 @@ source ~/.extra
 #
 # Install iterm shell integration
 # https://iterm2.com/documentation-shell-integration.html
-if [ ! -d ${HOME}/.iterm2_shell_integration.zsh ]; then
+if [[ ! -d ${HOME}/.iterm2_shell_integration.zsh ]]; then
     curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
     source ~/.iterm2_shell_integration.zsh
 fi
-#
 # Install oh-my-zsh
 # https://github.com/ohmyzsh/ohmyzsh
-if [ ! -d ${HOME}/.oh-my-zsh ]; then
+if [[ ! -d ${HOME}/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 #
-# Link the vim
-# Using https://github.com/amix/vimrc
-if [ ! -d ${HOME}/.vim_runtime ]; then
-    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-    sh ~/.vim_runtime/install_awesome_vimrc.sh
-    pushd ~/.vim_runtime
-        python update_plugins.py
-    popd
-    # mkdir -p ~/.config/nvim
-    # ln -sf ~/.vimrc ~/.config/nvim/init.vim
-    cp init/my_configs.vim ~/.vim_runtime
-
-    git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim_runtime/my_plugins/syntastic
-    git clone https://github.com/fatih/vim-go.git ~/.vim_runtime/my_plugins/vim-go
+# Install gvm
+# https://github.com/moovweb/gvm
+if [[ ! -d ${HOME}/.gvm ]]; then
+    curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | /bin/zsh -
 fi
-
+#
+# Lunarvim
+# Using https://www.lunarvim.org/
+if [[ ! -f ${HOME}/.local/bin/lvim ]]; then
+    curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh | /bin/zsh -
+fi
 # python
-if [ ! -d ${HOME}/.pyenv/versions ]; then
+# https://github.com/pyenv/pyenv
+if [[ ! -d ${HOME}/.pyenv/versions ]]; then
     PY_VER=$(pyenv install --list | grep "  3.10" | tail -n 1 | sed -e 's/ //g')
     pyenv install ${PY_VER}
     pyenv global ${PY_VER}
@@ -60,10 +55,10 @@ if [ ! -d ${HOME}/.pyenv/versions ]; then
 fi
 
 # https://github.com/jeffreytse/zsh-vi-mode
-if [ ! -d ${ZSH}/custom/plugins/zsh-vi-mode ]; then
+if [[ ! -d ${ZSH}/custom/plugins/zsh-vi-mode ]]; then
     git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH/custom/plugins/zsh-vi-mode
 fi
 
 # Return the .zshrc.pre back to .zshrc
 [ -f ${HOME}/.zshrc.pre* ] && mv ${HOME}/.zshrc.pre* ${HOME}/.zshrc
-
+[ -f ${HOME}/*brew.sh ] && rm -rf ${HOME}/*brew.sh
