e78b324c19a5871edf1d16a4e9d0544154c86c2d
Lim Swee Tat <slim@abnormalsecurity.com>
2022-11-29 22:18:55 +0800

    Sync with config at 2022-11-29 22:18:55+0800 by st_lim@tanjiro

--- bin/clean_abnormal_source.zsh
+++ bin/clean_abnormal_source.zsh
@@ -2,12 +2,20 @@
 
 pushd ${SOURCE}
   mv ${SOURCE}/.idea ${HOME}/idea-$(date +%Y%m%d)
+  for i in ${SOURCE}/src/js/abnormal/daffodil-playwright/.env*; do
+    mv ${i} ${HOME}/$(basename $i)-$(date +%Y%m%d)
+  done
   bazel clean
   git clean -xdf
   git gc
   git prune
   git pull
   mv ${HOME}/idea-$(date +%Y%m%d) ${SOURCE}/.idea
+  for i in ${HOME}/.env*; do
+    env_file_with_date = $(basename $i)
+    env_file = ${env_file_with_date/-$(date +%Y%m%d)/}
+    mv ${HOME}/${i} ${SOURCE}/src/js/abnormal/daffodil-playwright/${env_file}
+  done
   ${SOURCE}/bin/venv-sync
   pip install \
     debugpy \
