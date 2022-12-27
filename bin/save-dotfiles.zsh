a025051fc564655c19c11765030e51f7925d894f
Lim Swee Tat <slim@abnormalsecurity.com>
2022-12-27 14:36:32 +0800

    Sync with config at 2022-12-27 14:36:32+0800 by st_lim@tanjiro

--- bin/save-dotfiles.zsh
+++ bin/save-dotfiles.zsh
@@ -2,12 +2,11 @@
 DOTFILES_DIR=${HOME}/go/src/github.com/stlimtat/dotfiles
 rsync -ar --progress ${HOME}/.{abnormal,aliases,bash_profile,bash_prompt,bashrc,curlrc,devenv,exports,extra,functions,gitconfig,wgetrc,zshrc} ${DOTFILES_DIR}/
 rsync -ar --progress ${HOME}/bin/*.zsh ${DOTFILES_DIR}/bin/
-if [[ -d "${HOME}/.config/lvim" ]]; then
-  rsync -ar --progress --exclude="plugin" ${HOME}/.config/lvim/ ${DOTFILES_DIR}/.config/lvim/
-fi
-if [[ -d "${HOME}/.config/nvim/lua/user" ]]; then
-  rsync -ar --progress --exclude="plugin" ${HOME}/.config/nvim/lua/user/ ${DOTFILES_DIR}/.config/nvim/lua/user/
-fi
+for s in .config/lvim .config/nvim/lua/user .config/kitty; do
+  if [[ -d "${HOME}/${s}" ]]; then
+    rsync -ar --progress --exclude="plugin" ${HOME}/${s}/ ${DOTFILES_DIR}/${s}/
+  fi
+done
 pushd ${DOTFILES_DIR}
   git commit -m "Sync with config at $(date +'%F %T%z') by $(whoami)@$(hostname)" -a
   git push
