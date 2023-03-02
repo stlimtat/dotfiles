6f40edd0849c23205b230105299f7eb98aecc305
Lim Swee Tat <slim@abnormalsecurity.com>
2023-03-02 19:02:23 +0800

    Sync with config at 2023-03-02 19:02:23+0800 by st_lim@C71D4P6QGD.local

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -22,48 +22,11 @@ return {
   hyperlink_rules = {
     -- Linkify things that look like URLs and the host has a TLD name.
     -- Compiled-in default. Used if you don't specify any hyperlink_rules.
-    {
-      regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
-      format = '$0',
-    },
-
-    -- linkify email addresses
-    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
-    -- {
-    --   regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
-    --   format = 'mailto:$0',
-    -- },
-
-    -- file:// URI
-    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
-    -- {
-    --   regex = [[\bfile://\S*\b]],
-    --   format = '$0',
-    -- },
-
+    { regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',    format = '$0' },
     -- Linkify things that look like URLs with numeric addresses as hosts.
     -- E.g. http://127.0.0.1:8000 for a local development server,
     -- or http://192.168.1.1 for the web interface of many routers.
-    {
-      regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
-      format = '$0',
-    },
-
-    -- Make task numbers clickable
-    -- The first matched regex group is captured in $1.
-    -- {
-    --   regex = [[\b[tT](\d+)\b]],
-    --   format = 'https://example.com/tasks/?t=$1',
-    -- },
-
-    -- Make username/project paths clickable. This implies paths like the following are for GitHub.
-    -- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
-    -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
-    -- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
-    -- {
-    --   regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-    --   format = 'https://www.github.com/$1/$3',
-    -- },
+    { regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]], format = '$0' },
   },
   keys = {
     -- Clears only the scrollback and leaves the viewport intact.
@@ -71,20 +34,27 @@ return {
     -- be able to scroll back until you've output more stuff on screen.
     -- This is the default behavior.
     -- { key = 'K', mods = 'CTRL|SHIFT',  action = act.ClearScrollback 'ScrollbackOnly', },
-    { key = 'K', mods = 'SUPER|SHIFT', action = act.ClearScrollback 'ScrollbackOnly', },
+    { key = 'K',  mods = 'SUPER|SHIFT', action = act.ClearScrollback 'ScrollbackOnly', },
     -- Clears the scrollback and viewport leaving the prompt line the new first line.
     -- { key = 'K', mods = 'CTRL|SHIFT',  action = act.ClearScrollback 'ScrollbackAndViewport', },
-    { key = 'K', mods = 'SUPER|SHIFT', action = act.ClearScrollback 'ScrollbackAndViewport', },
+    { key = 'K',  mods = 'SUPER|SHIFT', action = act.ClearScrollback 'ScrollbackAndViewport', },
     -- Clears the scrollback and viewport, and then sends CTRL-L to ask the
     -- shell to redraw its prompt
     -- { key = 'K', mods = 'CTRL|SHIFT',  action = act.Multiple { act.ClearScrollback 'ScrollbackAndViewport', act.SendKey { key = 'L', mods = 'CTRL' }, }, },
-    { key = 'K', mods = 'SUPER|SHIFT', action = act.Multiple { act.ClearScrollback 'ScrollbackAndViewport', act.SendKey { key = 'L', mods = 'CTRL' }, }, },
-    { key = '"', mods = 'ALT|SHIFT',   action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
-    { key = "'", mods = 'ALT|SHIFT',   action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
-    { key = '%', mods = 'ALT|SHIFT',   action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-    { key = '5', mods = 'ALT|SHIFT',   action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-    { key = 'X', mods = 'SUPER|SHIFT', action = act.ActivateCopyMode },
-    { key = 'x', mods = 'SUPER|SHIFT', action = act.ActivateCopyMode },
+    { key = 'K',  mods = 'SUPER|SHIFT', action = act.Multiple { act.ClearScrollback 'ScrollbackAndViewport', act.SendKey { key = 'L', mods = 'CTRL' }, }, },
+    { key = '|',  mods = 'ALT|SHIFT',   action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = '\\', mods = 'ALT|SHIFT',   action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = '-',  mods = 'ALT|SHIFT',   action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = '_',  mods = 'ALT|SHIFT',   action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = 'w',  mods = 'CMD',         action = act.CloseCurrentPane({ confirm = true }) },
+    { key = 'X',  mods = 'SUPER|SHIFT', action = act.ActivateCopyMode },
+    { key = 'x',  mods = 'SUPER|SHIFT', action = act.ActivateCopyMode },
+    { key = 's',  mods = 'ALT|SHIFT',   action = act.PaneSelect({ mode = "SwapWithActive" }) },
+    { key = 'S',  mods = 'ALT|SHIFT',   action = act.PaneSelect({ mode = "SwapWithActive" }) },
+    { key = '{',  mods = 'ALT|SHIFT',   action = act.RotatePanes("CounterClockwise") },
+    { key = '[',  mods = 'ALT|SHIFT',   action = act.RotatePanes("CounterClockwise") },
+    { key = '}',  mods = 'ALT|SHIFT',   action = act.RotatePanes("Clockwise") },
+    { key = ']',  mods = 'ALT|SHIFT',   action = act.RotatePanes("Clockwise") },
   },
   -- How many lines of scrollback you want to retain per tab
   scrollback_lines = 3500,
