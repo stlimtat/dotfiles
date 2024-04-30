e2c8401b24a857897f4fe58f9e20c45442102de5
Lim Swee Tat <slim@abnormalsecurity.com>
2024-04-30 16:48:49 +0800

    Adding nvim and karabiner

---
+++ .config/nvim/lua/plugins/core.lua
@@ -0,0 +1,11 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "luisiacc/gruvbox-baby" },
+  { "rebelot/kanagawa.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
