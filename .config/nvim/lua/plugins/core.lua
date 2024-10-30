604cd12ddfbf93a636ac73bd1cee7b7f50a64532
Lim Swee Tat <slim@abnormalsecurity.com>
2024-10-30 16:43:59 +0800

    Sync with config at 2024-10-30 16:43:59+0800 by st_lim@mac-C71D4P6QGD

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,13 +1,12 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
-  { "luisiacc/gruvbox-baby" },
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- { "luisiacc/gruvbox-baby" },
   -- { "rebelot/kanagawa.nvim" },
   -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-  {
-    "LazyVim/LazyVim",
-    -- opts = {
-    --   colorscheme = "catppuccin",
-    -- },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
 }
