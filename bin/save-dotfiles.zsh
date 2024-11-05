d82e0500af82da7d878696afc0b350ac5762c9f5
Lim Swee Tat <st_lim@stlim.net>
2024-11-06 00:32:25 +0800

    Sync with config at 2024-11-06 00:32:25+0800 by st_lim@tanjiro

--- bin/save-dotfiles.zsh
+++ bin/save-dotfiles.zsh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 DOTFILES_DIR=${HOME}/go/src/github.com/stlimtat/dotfiles
-rsync -ar --progress ${HOME}/.{ab-alias,ab-fn,abnormal,aliases,bash_profile,bash_prompt,bashrc,curlrc,devenv,exports,extra,functions,gitconfig,ideavimrc,.psqlrc,wgetrc,zshrc} ${DOTFILES_DIR}/
+rsync -ar --progress ${HOME}/.{aliases,bash_profile,bash_prompt,bashrc,curlrc,devenv,exports,extra,functions,gitconfig,ideavimrc,.psqlrc,wgetrc,zshrc} ${DOTFILES_DIR}/
 rsync -ar --progress ${HOME}/bin/*.zsh ${DOTFILES_DIR}/bin/
 rsync -ar --progress ${HOME}/bin/{wezterm,}.sh ${DOTFILES_DIR}/bin/
-for s in .config/gh .config/lvim .config/nvim/after .config/nvim/lua .config/karabiner/karabiner.json .config/kitty .config/wezterm; do
+for s in .config/gh .config/nvim/after .config/nvim/lua .config/karabiner/karabiner.json .config/wezterm; do
   if [[ -d "${HOME}/${s}" ]]; then
     rsync -ar --progress --exclude="plugin" ${HOME}/${s}/ ${DOTFILES_DIR}/${s}/
   fi
