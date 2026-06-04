52d766aa79a2bcef4df90f3131d63c306950021b
Swee Tat Lim <st_lim@stlim.net>
2026-06-04 11:38:02 +0800

    Fixing some problems with claude

--- home/private_dot_config/private_op/private_plugins.sh
+++ home/private_dot_config/private_op/private_plugins.sh
@@ -2,3 +2,4 @@ export OP_PLUGIN_ALIASES_SOURCED=1
 if [[ -z "$CLAUDE_SESSION" ]]; then
   alias gh="op plugin run -- gh"
 fi
+alias claude="op run --env-file=\"${HOME}/.config/op/env\" -- $(brew --prefix)/bin/claude"
