82a1b566b24408e0872fd701206cf282bc54a0b7
Swee Tat Lim <st_lim@stlim.net>
2026-07-02 15:15:53 +0800

    Update bin/cleanUsb.zsh

--- home/private_bin/private_cleanUsb.zsh
+++ home/private_bin/private_cleanUsb.zsh
@@ -8,6 +8,7 @@ if [ "${USB}" = "" -o ! -d ${USB_PATH} ]; then
 fi
 
 sudo mdutil -i off -d ${USB_PATH}
+sudo rm -rf ${USB_PATH}/.{fseventsd,Spotlight-V*,Trashes} 2> /dev/null
 sudo rm -rf ${USB_PATH}/.{,_.}{fseventsd,Spotlight-V*,Trashes} 2> /dev/null
 mkdir ${USB_PATH}/.fseventsd
 touch ${USB_PATH}/.fseventsd/no_log ${USB_PATH}/.metadata_never_index ${USB_PATH}/.Trashes
